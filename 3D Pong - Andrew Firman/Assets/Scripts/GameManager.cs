62d11685aabfe50373a63f5e8e0dc1d96861db4b
andrewfirmansap <34363819+andrewfirmansap@users.noreply.github.com>
2022-06-23 09:52:38 +0700

    wall death

--- 3D Pong - Andrew Firman/Assets/Scripts/GameManager.cs
+++ 3D Pong - Andrew Firman/Assets/Scripts/GameManager.cs
@@ -11,12 +11,17 @@ public class GameManager : MonoBehaviour
     public GameObject paddle2;
     public GameObject paddle3;
     public GameObject paddle4;
+    public GameObject GoalP1;
+    public GameObject GoalP2;
+    public GameObject GoalP3;
+    public GameObject GoalP4;
     public Material paddleDiedMaterial;
     private bool P1isDead;
     private bool P2isDead;
     private bool P3isDead;
     private bool P4isDead;
     private float timer;
+    private int deathCount;
     public List<GameObject> ballTemplateList;
     private List<GameObject> ballList;
    
@@ -28,6 +33,7 @@ public class GameManager : MonoBehaviour
         P2isDead = false;
         P3isDead = false;
         P4isDead = false;
+        deathCount=0;
     }
 
     // Update is called once per frame
@@ -40,7 +46,11 @@ public class GameManager : MonoBehaviour
             SpawnRandomBall();
             timer -= spawnInterval;
         }
-        
+
+        if (deathCount == 3)
+        {
+            Debug.Log("Game Over");
+        }
 
     }
     public void SpawnRandomBall()
@@ -64,28 +74,40 @@ public class GameManager : MonoBehaviour
         {
             paddle1.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
             paddle1.GetComponent<MeshRenderer>().material = paddleDiedMaterial;
+            GoalP1.GetComponent<Collider>().isTrigger= false;
+            P1isDead = true;
             Debug.Log("Paddle 1 died!");
+            deathCount = deathCount + 1;
 
         }
         else if (paddleNum == 2)
         {
             paddle2.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
             paddle2.GetComponent<MeshRenderer>().material = paddleDiedMaterial;
+            GoalP2.GetComponent<Collider>().isTrigger = false;
+            P2isDead = true;
             Debug.Log("Paddle 2 died!");
+            deathCount = deathCount + 1;
 
         }
         else if (paddleNum == 3)
         {
             paddle3.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
             paddle3.GetComponent<MeshRenderer>().material = paddleDiedMaterial;
+            GoalP3.GetComponent<Collider>().isTrigger = false;
+            P3isDead = true;
             Debug.Log("Paddle 3 died!");
+            deathCount = deathCount + 1;
 
         }
         else if (paddleNum == 4)
         {
             paddle4.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
             paddle4.GetComponent<MeshRenderer>().material = paddleDiedMaterial;
+            GoalP4.GetComponent<Collider>().isTrigger = false;
+            P4isDead = true;
             Debug.Log("Paddle 4 died!");
+            deathCount = deathCount + 1;
 
         }
     }
